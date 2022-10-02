7d595989747deb72e9b1f722572311bd3fa32a5f
Black <modnyak0001@mail.ru>
2022-10-03 01:54:34 +1000

    Solved Problem 7

---
+++ 01.10.22/Problem 7/Program.cs
@@ -0,0 +1,5 @@
+﻿//Показать последнюю цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine()?? "0");
+
+Console.WriteLine("Последнее число: " + number%10);
