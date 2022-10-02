1b2ee11d22b17ea14ffcac75424e344865b982f5
Black <modnyak0001@mail.ru>
2022-10-02 19:00:38 +1000

    Problem 1 Solved

--- 01.10.22/Problem 1/Program.cs
+++ 01.10.22/Problem 1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()?? "0");
+if (Math.Pow(a, 2) == b) Console.Write(a+"^2 = "+b);
+else Console.Write(a+"^2 != "+b);
