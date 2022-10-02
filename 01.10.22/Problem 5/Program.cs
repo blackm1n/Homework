c5bb346fb263c6019cbecda58a02dfe09d789cd8
Black <modnyak0001@mail.ru>
2022-10-03 01:55:13 +1000

    Shift+Ctrl+F Problem 5, 6, 7

--- 01.10.22/Problem 5/Program.cs
+++ 01.10.22/Problem 5/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Показать числа от -N до N
 Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine()?? "0");
+int number = int.Parse(Console.ReadLine() ?? "0");
 int i = -number;
 
 while (i <= number)
--- 01.10.22/Problem 6/Program.cs
+++ 01.10.22/Problem 6/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Показать четные числа от 1 до N
 Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine()?? "0");
+int number = int.Parse(Console.ReadLine() ?? "0");
 int i = 1;
 
 while (i < number)
