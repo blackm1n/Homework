9d618fb1dd9fa0968d11b87330ced95fd912b49b
Black <modnyak0001@mail.ru>
2022-10-03 01:50:47 +1000

    Solved Problem 6

---
+++ 01.10.22/Problem 6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать четные числа от 1 до N
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()?? "0");
+int i = 1;
+while (i < number)
+{
+    if (i % 2 == 0) Console.Write(i + " ");
+    i++;
+}
