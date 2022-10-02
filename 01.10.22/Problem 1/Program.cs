673331327e1952ef6031f7d30d7d52b0c8f31078
Black <modnyak0001@mail.ru>
2022-10-02 21:17:09 +1000

    Shift+Ctrl+F Problem 1

--- 01.10.22/Problem 1/Program.cs
+++ 01.10.22/Problem 1/Program.cs
@@ -1,7 +1,7 @@
 ﻿//По двум заданным числам проверять является ли первое квадратом второго
 Console.Write("Введите первое число: ");
-double a = double.Parse(Console.ReadLine()?? "0");
+double a = double.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите второе число: ");
-double b = double.Parse(Console.ReadLine()?? "0");
-if (Math.Pow(b, 2) == a) Console.Write(b+"^2 = "+a);
-else Console.Write(b+"^2 != "+a);
+double b = double.Parse(Console.ReadLine() ?? "0");
+if (Math.Pow(b, 2) == a) Console.Write(b + "^2 = " + a);
+else Console.Write(b + "^2 != " + a);
