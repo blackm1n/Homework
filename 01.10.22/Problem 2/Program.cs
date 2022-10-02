59942b3edb517c8845bf39d073c90e460813d826
Black <modnyak0001@mail.ru>
2022-10-03 01:33:28 +1000

    Problem 2 solved

---
+++ 01.10.22/Problem 2/Program.cs
@@ -0,0 +1,6 @@
+﻿// По заданному номеру дня недели вывести его название
+string[] day = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+Console.Write("Введите номер дня: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+if (number > 0 && number < 8) Console.WriteLine(day[number-1]);
+else Console.WriteLine("Не является номером дня");
