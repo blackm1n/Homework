67eeb479915791bc5b254804306a7f8030543624
Black <modnyak0001@mail.ru>
2022-10-03 01:42:21 +1000

    Problem 3 solved

---
+++ 01.10.22/Problem 3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти максимальное из трех чисел
+double Max(double a, double b, double c)
+{
+    double max = 0;
+    if (a > max) max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max;
+}
+
+Console.Write("Введите первое число: ");
+double a = double.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+double b = double.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+double c = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Максимальное: " + Max(a, b, c));
