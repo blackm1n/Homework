b6b235ad8955554831879c85a451433d9bce950e
Black <modnyak0001@mail.ru>
2022-10-03 19:13:23 +1000

    Solved Problem 11

---
+++ 01.10.22/Problem 11/Program.cs
@@ -0,0 +1,8 @@
+﻿//Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите заданное число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+if (a % b == 0) Console.WriteLine("Число кратно");
+else Console.WriteLine("Число не кратно. Остаток: " + a % b);
