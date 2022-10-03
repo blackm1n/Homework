8c2d7d57a3f3450b8336690203542f7a97125d47
Black <modnyak0001@mail.ru>
2022-10-03 19:33:54 +1000

    Solved Problem 4

---
+++ 02.10.22/Problem 4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Программа проверяет пятизначное число на палиндромом.
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int fhalf = number / 1000;
+int shalf = number % 100;
+
+if (fhalf / 10 == shalf % 10 && fhalf % 10 == shalf / 10) Console.WriteLine("Число является палиндромом");
+else Console.WriteLine("Число не является палиндромом");
