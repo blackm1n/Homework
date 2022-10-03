0d7ae217265c89c1b9fff4c2915f48b8f88b2a3c
Black <modnyak0001@mail.ru>
2022-10-03 19:00:33 +1000

    Solved Problem 9

---
+++ 01.10.22/Problem 9/Program.cs
@@ -0,0 +1,6 @@
+﻿//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.Write("Введите двухзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if (number / 10 > number % 10) Console.WriteLine("Наибольшая цифра числа: " + number / 10);
+else Console.WriteLine("Наибольшая цифра числа: " + number % 10);
