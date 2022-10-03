a9e1ad256bd39f07d4095bd282b637987887d8f5
Black <modnyak0001@mail.ru>
2022-10-03 23:46:51 +1000

    Fixed Problem 12

--- 01.10.22/Problem 12/Program.cs
+++ 01.10.22/Problem 12/Program.cs
@@ -2,7 +2,9 @@
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine() ?? "0");
 
-while (number > 1000) number = number / 10;
-
-if (number > 100) Console.WriteLine("Третье число: " + number % 10);
+if (number > 100) 
+{
+    while (number > 1000) number = number / 10;
+    Console.WriteLine("Третье число: " + number % 10);
+}
 else Console.WriteLine("Третье число не существует");
