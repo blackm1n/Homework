31a672ad69b9505dbb323d80cc1fc2cd315bccc7
Black <modnyak0001@mail.ru>
2022-10-03 19:03:30 +1000

    Solved Problem 10

---
+++ 01.10.22/Problem 10/Program.cs
@@ -0,0 +1,7 @@
+﻿//Удалить вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+int newnumber = (number / 100) * 10 + number % 10;
+
+Console.WriteLine("Ваше число: " + newnumber);
