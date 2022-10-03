4cfa63bd88898a6ec5023fff0f930ee53b538aeb
Black <modnyak0001@mail.ru>
2022-10-03 18:58:05 +1000

    Solved Problem 8

---
+++ 01.10.22/Problem 8/Program.cs
@@ -0,0 +1,5 @@
+﻿//Показать вторую цифру трёхзначного числа
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Второе число: " + (number / 10) % 10);
