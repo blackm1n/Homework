53bb775ec7fcc95a7ab31cc2c2281a377b919c18
Black <modnyak0001@mail.ru>
2022-10-03 19:22:21 +1000

    Solved Problem 2

---
+++ 02.10.22/Problem 2/Program.cs
@@ -0,0 +1,17 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другог
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+int min = a;
+int max = b;
+
+if (a > b)
+{
+    min = b;
+    max = a;
+} 
+
+if (Math.Pow(min, 2) == max) Console.WriteLine(min + "^2 = " + max);
+else Console.WriteLine(min + "^2 != " + max);
