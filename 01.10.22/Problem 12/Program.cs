95e9dc7d73dee8f37544a8be4acebf354850e3fb
Black <modnyak0001@mail.ru>
2022-10-03 19:15:30 +1000

    Solved Problem 12

---
+++ 01.10.22/Problem 12/Program.cs
@@ -0,0 +1,6 @@
+﻿//Найти третью цифру числа или сообщить, что её нет
+Console.Write("Введите число: ");
+string number = Console.ReadLine() ?? "0";
+
+if (number.Length > 2) Console.WriteLine(number[2]);
+else Console.WriteLine("Третье число не существует");
