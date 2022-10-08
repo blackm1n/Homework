f6ca519a3248dfd73301677d8e0e5716b024cd96
Black <modnyak0001@mail.ru>
2022-10-08 23:00:10 +1000

    Copied from Seminar with slight adjustments

---
+++ 08.10.22/Problem 3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int index = 1;
+int result = 1;
+while (index <= number)
+{
+    result = result * index;
+    index++;
+}
+Console.WriteLine(number + "! = " + result);
