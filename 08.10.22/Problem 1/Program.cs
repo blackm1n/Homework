d326a3ea7836f51ccbb49a7d426941fe973bdb17
Black <modnyak0001@mail.ru>
2022-10-08 22:57:44 +1000

    Copied from Seminar with slight adjustments

---
+++ 08.10.22/Problem 1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Найти кубы чисел от 1 до N
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int index = 1;
+while (index <= number)
+{
+    Console.WriteLine(index + "^2 = " + Math.Pow(index, 3));
+    index++;
+}
