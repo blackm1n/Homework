888f44fdf0378bd961f22228d077a47cfeb5223f
Black <modnyak0001@mail.ru>
2022-10-08 23:03:22 +1000

    Copied from Problem 1 with slight adjustments

---
+++ 08.10.22/Problem 4/Program.cs
@@ -0,0 +1,11 @@
+﻿//Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int index = 1;
+double cube = 0;
+while (index <= number)
+{
+    cube = Math.Pow(index, 3);
+    if (cube % 2 == 0) Console.WriteLine(index + "^3 = " + cube);
+    index++;
+}
