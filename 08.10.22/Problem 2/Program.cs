f24fdacd0bfa7a60437e1a99c270ffe326e8fb3c
Black <modnyak0001@mail.ru>
2022-10-12 21:44:45 +1000

    Solved Problem 2

---
+++ 08.10.22/Problem 2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Подсчитать сумму цифр в числе
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+
+while (number != 0)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+}
+
+Console.WriteLine("Сумма цифр: " + sum);
