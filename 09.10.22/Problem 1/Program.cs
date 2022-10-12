93fd1e9082a52a330d61b25048c436415de30c50
Black <modnyak0001@mail.ru>
2022-10-12 21:46:35 +1000

    Copied from Seminar with slight adjustments

---
+++ 09.10.22/Problem 1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0,2);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
