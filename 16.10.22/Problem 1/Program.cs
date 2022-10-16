496c5243a39ae4c62cbf1a27a1a403eb5efedae1
Black <modnyak0001@mail.ru>
2022-10-17 03:31:35 +1000

    Copied from Seminar with slight adjustments

---
+++ 16.10.22/Problem 1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Показать двумерный массив размером m×n заполненный вещественными числами
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" {array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] array)
+{
+    var random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-9, 10) + random.NextDouble();
+        }
+    }
+}
+
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+double[,] array = new double[m, n];
+
+FillArray(array);
+PrintArray(array);
