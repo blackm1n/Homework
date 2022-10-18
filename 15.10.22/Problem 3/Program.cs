ea13d2eb0c5f4bb79bc4683334af96f503b6f0da
Black <modnyak0001@mail.ru>
2022-10-18 19:21:16 +1000

    Solved Problem 3

---
+++ 15.10.22/Problem 3/Program.cs
@@ -0,0 +1,40 @@
+﻿//Написать программу копирования массива
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[] array)
+{
+    var random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-9, 9);
+    }
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] arraycopy = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arraycopy[i] = array[i];
+    }
+    return arraycopy;
+}
+
+Console.Write("Введите размер массива: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+
+int[] array = new int[m];
+
+FillArray(array);
+Console.Write("Ориг. - ");
+PrintArray(array);
+int[] array2 = CopyArray(array);
+Console.Write("Копия - ");
+PrintArray(array2);
