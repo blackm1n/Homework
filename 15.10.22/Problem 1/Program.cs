1defb0766b02a2b4ae61a0a2e70f13b409233cef
Black <modnyak0001@mail.ru>
2022-10-20 22:38:55 +1000

    Changed int to double

--- 15.10.22/Problem 1/Program.cs
+++ 15.10.22/Problem 1/Program.cs
@@ -1,12 +1,12 @@
 ﻿//Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
 Console.Write("Введите k1: ");
-double k1 = int.Parse(Console.ReadLine() ?? "0");
+double k1 = double.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите b1: ");
-double b1 = int.Parse(Console.ReadLine() ?? "0");
+double b1 = double.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите k2: ");
-double k2 = int.Parse(Console.ReadLine() ?? "0");
+double k2 = double.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите b2: ");
-double b2 = int.Parse(Console.ReadLine() ?? "0");
+double b2 = double.Parse(Console.ReadLine() ?? "0");
 
 double x = (b2 - b1)/(k1 - k2);
 
