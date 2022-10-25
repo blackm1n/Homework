061d77a912ab7a305e0fcb0e560c9d09ea1edffd
Black <modnyak0001@mail.ru>
2022-10-25 16:48:55 +1000

    Solved Problem 1

---
+++ 22.10.22/Problem 1/Program.cs
@@ -0,0 +1,58 @@
+﻿//Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    var random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(-20, 21);
+        }
+    }
+}
+
+void SortArray(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[m, n];
+
+FillArray(array);
+Console.WriteLine("До");
+PrintArray(array);
+SortArray(array);
+Console.WriteLine("После");
+PrintArray(array);
