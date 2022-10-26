852ed1a1732336c9aa6fbc73d80f4f34376c6231
Black <modnyak0001@mail.ru>
2022-10-26 20:26:22 +1000

    Actually Solved Problem 1

--- 23.10.22/Problem 1/Program.cs
+++ 23.10.22/Problem 1/Program.cs
@@ -1,8 +1,7 @@
 ﻿//Найти произведение двух матриц
 //Я не знаю точно, что вы имеете ввиду под "произведением двух матриц". 
-//Как по мне это звучит что надо каждый элемент одной матрицы, 
-//умножить на соответсующий элемент другой матрицы.
-//Надеюсь, что это то, что вы имели ввиду.
+//Сам я понимаю как произведение каждого элемента первой матрицы на произведение соответсующего элемента другой матрицы.
+//Но в интернете совершенно другое понимание, которое я оставлю как главное решение, а свое как побочное. Оно будет закоментировано.
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -22,13 +21,39 @@ void FillArray(int[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = random.Next(-20, 21);
+            array[i, j] = random.Next(1, 10);
         }
     }
 }
 
-int[,] MultiplicationOfMatrixes(int[,] array, int[,] array2)
+//Главное решение задачи.
+int[,]? MultiplicationOfMatrixes(int[,] array, int[,] array2)
 {
+    if (array.GetLength(1) != array2.GetLength(0))
+    {
+        return null;
+    }
+    int[,] result = new int[array.GetLength(0), array2.GetLength(1)];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(0); k++)
+            {
+                result[i, j] += array[i, k] * array2[k, j];
+            }
+        }
+    }
+    return result;
+}
+
+//Мое понимание задания до просмотра интернета.
+/*int[,]? MultiplicationOfMatrixes(int[,] array, int[,] array2)
+{
+    if (array.GetLength(0) != array2.GetLength(0) || array.GetLength(1) != array2.GetLength(1))
+    {
+        return null;
+    }
     int[,] result = new int[array.GetLength(0), array.GetLength(1)];
     for (int i = 0; i < result.GetLength(0); i++)
     {
@@ -38,15 +63,20 @@ int[,] MultiplicationOfMatrixes(int[,] array, int[,] array2)
         }
     }
     return result;
-}
+}*/
 
-Console.Write("Введите m: ");
-int m = int.Parse(Console.ReadLine() ?? "0");
-Console.Write("Введите n: ");
-int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите m первой матрицы: ");
+int m1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите n первой матрицы: ");
+int n1 = int.Parse(Console.ReadLine() ?? "0");
 
-int[,] array = new int[m, n];
-int[,] array2 = new int[m, n];
+Console.Write("Введите m второй матрицы: ");
+int m2 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите n второй матрицы: ");
+int n2 = int.Parse(Console.ReadLine() ?? "0");
+
+int[,] array = new int[m1, n1];
+int[,] array2 = new int[m2, n2];
 
 FillArray(array);
 FillArray(array2);
@@ -55,6 +85,11 @@ PrintArray(array);
 Console.WriteLine("Вторая матрица:");
 PrintArray(array2);
 
-int[,] result = MultiplicationOfMatrixes(array, array2);
-Console.WriteLine("Произведение матриц:");
-PrintArray(result);
+int[,]? result = MultiplicationOfMatrixes(array, array2);
+if (result != null)
+{
+    Console.WriteLine("Произведение матриц:");
+    PrintArray(result);
+}
+else
+    Console.WriteLine("Невозможно произвести произведение матриц");
