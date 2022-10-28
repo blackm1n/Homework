51fd8b5574a306cc09a8c32bd6adaa65c1cf5424
Black <modnyak0001@mail.ru>
2022-10-28 19:00:10 +1000

    Added comments and alternative solve

--- 23.10.22/Problem 3/Program.cs
+++ 23.10.22/Problem 3/Program.cs
@@ -1,4 +1,6 @@
 ﻿//Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+//Задание можно было сделать просто прибавляв один к каждому проследующему элементу... Но я решил сделать через рандом...
+//Дополнительное решение в комментариях.
 void PrintArrayWithIndexes(int[,,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -12,6 +14,22 @@ void PrintArrayWithIndexes(int[,,] array)
         }
     }
 }
+
+/*void FillArrayWithoutRepeat(int[,,] array)
+{
+    int n = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = n;
+                n++;
+            }
+        }
+    }
+}*/
 
 void FillArrayWithoutRepeat(int[,,] array)
 {
