89af869ff41904750f66d135c0fffb7122c8eaea
Black <modnyak0001@mail.ru>
2022-11-14 21:05:45 +1000

    Solved Problem 1

---
+++ 12.11.22/Problem 1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Показать натуральные числа от M до N, N и M заданы
+void ShowNumbers(int M, int N)
+{
+    if (N > M)
+        ShowNumbers(M, N - 1);
+    Console.Write($"{N} ");
+}
+
+Console.Write("Введите M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+ShowNumbers(M, N);
