5a42521d6b7a14403bcb730d2268a7aefc7c94dc
Black <modnyak0001@mail.ru>
2022-11-14 21:21:43 +1000

    Solved Problem 3

---
+++ 12.11.22/Problem 3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Написать программу вычисления функции Аккермана
+int Akkerman(int m, int n)
+{
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else if (m > 0 && n > 0)
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    else
+        return n + 1;
+}
+
+Console.Write("Введите m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine(Akkerman(m, n));
