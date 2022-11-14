e8a2b20d8ce8eb14d767288498d15af4a7607b29
Black <modnyak0001@mail.ru>
2022-11-14 21:08:28 +1000

    Solved Problem 2

---
+++ 12.11.22/Problem 2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Найти сумму элементов от M до N, N и M заданы
+int SumNumbers(int M, int N)
+{
+    int sum = N;
+    if (N > M)
+        sum += SumNumbers(M, N - 1);
+    return sum;
+}
+
+Console.Write("Введите M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine(SumNumbers(M, N));
