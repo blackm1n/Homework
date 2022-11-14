b6172e2509fb99a034a10f16bf81eebec8bf6902
Black <modnyak0001@mail.ru>
2022-11-14 21:27:17 +1000

    Solved Problem 4

---
+++ 12.11.22/Problem 4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+void CustomFibonacci(int N, int a, int b)
+{
+    Console.Write($"{a} ");
+    if (N > 1)
+        CustomFibonacci(N - 1, b, a+b);
+}
+
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите a: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите b: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+
+CustomFibonacci(N, a, b);
