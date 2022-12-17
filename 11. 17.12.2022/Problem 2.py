f72e167032f64de543c97251a49c14f57bc0c199
Black <modnyak0001@mail.ru>
2022-12-17 21:47:34 +1000

    Ctrl + Alt + L the code

--- 11. 17.12.2022/Problem 2.py
+++ 11. 17.12.2022/Problem 2.py
@@ -3,8 +3,8 @@
 num = int(input("Введите число: "))
 
 factorial = [1]
-for i in range(2, num+1):
-    result = i * factorial[i-2]
+for i in range(2, num + 1):
+    result = i * factorial[i - 2]
     factorial.append(result)
 
 print(factorial)
