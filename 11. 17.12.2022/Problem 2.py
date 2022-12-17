d872ee56c1867ea5bfeb7a8ca185d35dc5e29ef9
Black <modnyak0001@mail.ru>
2022-12-17 21:32:32 +1000

    Solved Problem 2

---
+++ 11. 17.12.2022/Problem 2.py
@@ -0,0 +1,10 @@
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+num = int(input("Введите число: "))
+
+factorial = [1]
+for i in range(2, num+1):
+    result = i * factorial[i-2]
+    factorial.append(result)
+
+print(factorial)
