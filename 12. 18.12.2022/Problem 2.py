fe5ed14cb9acc4601f8f28d73d8843cda5064f1f
Black <modnyak0001@mail.ru>
2022-12-18 19:59:22 +1000

    Solved Problem 2

---
+++ 12. 18.12.2022/Problem 2.py
@@ -0,0 +1,9 @@
+# Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+table = list(map(int, input("Введите числа: ").split(", ")))
+
+result = []
+for i in range(len(table)//2 + 1):
+    result.append(table[i] * table[-i - 1])
+
+print(result)
