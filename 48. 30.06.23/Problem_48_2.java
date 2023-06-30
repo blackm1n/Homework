8ec5f6efc43305d04fc55f9b0e15f09170c510cb
Black <modnyak0001@mail.ru>
2023-07-01 04:25:45 +1000

    Added additional exception in Problem 2

--- 48. 30.06.23/Problem_48_2.java
+++ 48. 30.06.23/Problem_48_2.java
@@ -2,15 +2,17 @@
 
 public class Problem_48_2 {
     public static void main(String[] args) {
-        String[][] arr1 = {{"1", "2", "3"}, {"4", "5", "6"}};
-        String[][] arr2 = {{"hello", "this", "is"}, {"a", "string", "array"}};
+        String[][] arr1 = null;
+        String[][] arr2 = {{"1", "2", "3"}, {"4", "5", "6"}};
+        String[][] arr3 = {{"hello", "this", "is"}, {"a", "string", "array"}};
         // System.out.println(sum2d(arr1));
         // System.out.println(sum2d(arr2));
+        // System.out.println(sum2d(arr3));
     }
 
     public static int sum2d(String[][] arr) {
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) { // NullPointerException
             for (int j = 0; j < 5; j++) { // ArrayIndexOutOfBoundsException
                 int val = Integer.parseInt(arr[i][j]); // NumberFormatException
                 sum += val;
