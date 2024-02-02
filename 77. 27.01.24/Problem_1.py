c04306e304766af40141b85b9889acf72483bb2a
Black <modnyak0001@mail.ru>
2024-02-03 01:29:30 +1000

    Fixed errors and added _is_active to Task

--- 77. 27.01.24/Problem_1.py
+++ 77. 27.01.24/Problem_1.py
@@ -22,8 +22,15 @@ class Task(pydantic.BaseModel):
     name: str
     description: str
     complete: bool
+    _is_active: bool = True
 
     _unique_id: classmethod = unique_validator("task_id")
+
+
+class TaskData(pydantic.BaseModel):
+    name: str
+    description: str
+    complete: bool
 
 
 app = FastAPI()
@@ -33,12 +40,12 @@ tasks = []
 
 @app.get("/tasks/")
 async def get_tasks():
-    return tasks
+    return [task for task in tasks if task._is_active]
 
 
 @app.get("/tasks/{task_id}")
 async def get_task_id(task_id: int):
-    return [task for task in tasks if task.task_id == task_id][0]
+    return [task for task in tasks if task.task_id == task_id and task._is_active]
 
 
 @app.post("/tasks/")
@@ -49,15 +56,18 @@ async def post_task(task: Task):
 
 
 @app.put("/tasks/{task_id}")
-async def update_task(task_id: int, new_task: Task):
+async def update_task(task_id: int, new_task: TaskData):
     for i in range(len(tasks)):
         if tasks[i].task_id == task_id:
-            tasks[i] = new_task
+            tasks[i].name = new_task.name
+            tasks[i].description = new_task.description
+            tasks[i].complete = new_task.complete
     return new_task
 
 
 @app.delete("/tasks/{task_id}")
 async def delete_task(task_id: int):
-    task = [task for task in tasks if task.task_id == task_id][0]
-    tasks.remove(task)
+    task = [task for task in tasks if task.task_id == task_id and task._is_active]
+    for t in task:
+        t._is_active = False
     return task
