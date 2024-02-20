a2a731d8a569d34a40eda1b3e7d43800cb840880
Black <modnyak0001@mail.ru>
2024-02-20 21:49:19 +1000

    Removed 07.02.24 and 10.02.24 because fuck me

--- 80. 10.02.24/Problem_1/app/management/commands/get_orders.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from ...models import Order
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **kwargs):
-        orders = Order.objects.all()
-        self.stdout.write(f'{orders}')
