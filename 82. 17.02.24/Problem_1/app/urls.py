1f78c6c48a0575321dce7dd57f6d8bba0f0da582
Black <modnyak0001@mail.ru>
2024-02-24 00:31:04 +1000

    Did Homework for 17.02.24

---
+++ 82. 17.02.24/Problem_1/app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.my_view, name='index'),
+    path('client_orders/<int:client_id>/', views.client_orders, name='client_orders'),
+    path('client_products/<str:length>/<int:client_id>/', views.client_products, name='client_products'),
+    path('product_image/', views.product_image, name='product_image'),
+]
